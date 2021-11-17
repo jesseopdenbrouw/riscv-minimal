ebb5b32c98dd5067f9803d53f243badf2853e44e
Jesse op den Brouw <51508208+jesseopdenbrouw@users.noreply.github.com>
2021-11-17 10:48:48 +0100

    Test global

--- CODE/flash/flash.c
+++ CODE/flash/flash.c
@@ -2,10 +2,14 @@
 
 #include "io.h"
 
+volatile uint32_t x;
+
 int main(void) {
 
 	volatile uint32_t *output = (uint32_t *) GPIO_DATAOUT;
 	volatile uint32_t counter;
+
+	x = x + 1;
 
 	while (1) {
 		for (counter = 0; counter < 5000000; counter ++);
