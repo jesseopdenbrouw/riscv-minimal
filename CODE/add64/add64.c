b906b9aaf44a0aecfdcb02f1be77fc16208bdf9b
Jesse op den Brouw <51508208+jesseopdenbrouw@users.noreply.github.com>
2021-11-17 10:46:29 +0100

    Added some variables for testing

--- CODE/add64/add64.c
+++ CODE/add64/add64.c
@@ -1,12 +1,21 @@
-volatile long long int r;
+volatile long long int r = 4;
+
+volatile long int w;
+volatile long int x;
+volatile long int y;
+volatile long int z;
+
+volatile long int ary[] = { 2, 5 ,7, 8 , 9};
 
 int main(void) {
 
-	register long long int a = 3, b = 5;
+	//register long long int a = 14, b = 7;
 
-	r = a + b;
+	//r = a + b;
 
-	r = r << 1;
+	r = r + r;
+
+	z = z + z;
 
 	return 0;
 }
