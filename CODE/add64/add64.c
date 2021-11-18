62090c3e857a381b2daae75b0acb78125050a913
Jesse op den Brouw <51508208+jesseopdenbrouw@users.noreply.github.com>
2021-11-18 15:08:54 +0100

    Changed

--- CODE/add64/add64.c
+++ CODE/add64/add64.c
@@ -1,11 +1,11 @@
-volatile long long int r = 4;
-
 volatile long int w;
 volatile long int x;
 volatile long int y;
-volatile long int z;
+volatile long int z = -1;
 
-volatile long int ary[] = { 2, 5 ,7, 8 , 9};
+volatile long long int r = 4;
+
+long int ary[] = { 2, 5 ,7, 8 , 9};
 
 int main(void) {
 
@@ -13,6 +13,8 @@ int main(void) {
 
 	//r = a + b;
 
+	z = -1;
+	
 	r = r + r;
 
 	z = z + z;
