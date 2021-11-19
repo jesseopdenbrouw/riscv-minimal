ce02e5d1e75a2b6f0aff330057a25875bc7b34b6
Jesse op den Brouw <51508208+jesseopdenbrouw@users.noreply.github.com>
2021-11-19 09:49:21 +0100

    changed

--- CODE/testio/testio.c
+++ CODE/testio/testio.c
@@ -3,8 +3,8 @@
 #include "io.h"
 
 int main(void) {
-	volatile uint32_t *input = (uint32_t *) GPIO_DATAIN;
-	volatile uint32_t *output = (uint32_t *) GPIO_DATAOUT;
+	volatile uint32_t *input = (uint32_t *) GPIO_DATAIN_ADDR;
+	volatile uint32_t *output = (uint32_t *) GPIO_DATAOUT_ADDR;
 
 	while (1) {
 		*output = *input;
