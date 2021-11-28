a3e66eef4b2ab65cf8325150566ce948c3dea776
Jesse op den Brouw <51508208+jesseopdenbrouw@users.noreply.github.com>
2021-11-28 16:06:53 +0100

    Update test.c

--- CODE/syscalls/test.c
+++ CODE/syscalls/test.c
@@ -1,12 +1,24 @@
 #include <stdio.h>
 #include <string.h>
+#include <malloc.h>
 
 #include "syscalls.h"
 
 int main(void) {
 
-	char str[30];
+	//char str[30];
 
-	_read(0, str, 1);
+//	errno = -1;
 
+//	char *p = (char *) malloc(100);
+
+/*
+ * for (int i = 0; i < 100; i++) {
+		p[i] = 'A';
+	}
+	*/
+
+	volatile float f = 1.0;
+
+	f = f + 2.0f;
 }
