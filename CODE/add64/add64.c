1e846bc263080ffbbd8a73ca14dd80267841c63a
Jesse op den Brouw <51508208+jesseopdenbrouw@users.noreply.github.com>
2021-11-28 16:04:21 +0100

    Update add64.c

--- CODE/add64/add64.c
+++ CODE/add64/add64.c
@@ -1,27 +1,8 @@
-volatile long int w;
-volatile long int x;
-volatile long int y;
-volatile long int z = -1;
-
-volatile long long int r = 4;
-
-long int ary[] = { 2, 5 ,7, 8 , 9};
-
-short int k = 4;
-
-char j = 'A';
-
 int main(void) {
 
-	//register long long int a = 14, b = 7;
+	register long long int r, a = 14, b = 7;
 
-	//r = a + b;
-
-	z = -1;
-	
-	r = r + r;
-
-	z = z + z;
+	r = a + b;
 
 	return 0;
 }
