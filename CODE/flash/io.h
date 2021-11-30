5f6fc686072abbc71aa1007a01e8285efe10f282
Jesse op den Brouw <51508208+jesseopdenbrouw@users.noreply.github.com>
2021-11-30 11:23:35 +0100

    Make #defines pointers to I/O

--- CODE/flash/io.h
+++ CODE/flash/io.h
@@ -4,7 +4,11 @@
 #ifndef _IO_H
 #define _IO_H
 
-#define GPIO_DATAIN_ADDR (0xf0000000)
-#define GPIO_DATAOUT_ADDR (0xf0000004)
+#include <stdint.h>
+
+#define IO_BASE (0xf0000000UL)
+
+#define GPIO_PINA (*(volatile uint32_t*)(IO_BASE+0x00000000UL))
+#define GPIO_POUTA (*(volatile uint32_t*)(IO_BASE+0x00000004UL))
 
 #endif
