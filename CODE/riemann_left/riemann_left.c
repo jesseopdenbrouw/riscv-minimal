21585939f23b37ee75c53ed27c204a22f9363a83
Jesse op den Brouw <51508208+jesseopdenbrouw@users.noreply.github.com>
2021-12-11 12:39:15 +0100

    Riemann Left Sum integration of sin^2(x)

---
+++ CODE/riemann_left/riemann_left.c
@@ -0,0 +1,36 @@
+/*
+ * riemannn_left -- integration using Riemann Left Sum
+ *
+ * Set the steps in variable n
+ *
+ * For 10 steps, run the simulation for 3200 us.
+ * Link with -lm
+ */
+
+
+#include <math.h>
+
+float f(float x) {
+    float y;
+    y = sinf(x);
+    return y*y;
+}
+
+int main() {
+
+    int k, n = 10;
+
+    float a = 0.0f;
+    float b = 6.28318530717958647692f;
+
+    float deltax = (b - a) / n;
+
+    /* View in RAM */
+    volatile float sum = 0.0f;
+
+    for (k = 0; k < n; k++) {
+        sum = sum + f(a + k*deltax) * deltax;
+    }
+
+    return 0;
+}
