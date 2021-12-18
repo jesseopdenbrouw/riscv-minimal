8a1ada9692160c66a8e0187c6de0970e9447c928
Jesse op den Brouw <51508208+jesseopdenbrouw@users.noreply.github.com>
2021-12-18 13:00:50 +0100

    Added writing addresses

--- CODE/monitor/monitor.c
+++ CODE/monitor/monitor.c
@@ -106,7 +106,7 @@ uint32_t read_address(char *s) {
 
 	uint32_t val = 0;
 
-	while (*s == ' ') {
+	while (isspace((int) *s)) {
 		s++;
 	}
 
@@ -114,7 +114,7 @@ uint32_t read_address(char *s) {
 		return 0;
 	}
 
-	while (*s != '\0') {
+	while (*s != '\0' && *s != ' ') {
 		if (isxdigit((int) *s)) {
 			if (isdigit((int) *s)) {
 				val = val*16 + *s - '0';
@@ -128,66 +128,152 @@ uint32_t read_address(char *s) {
 	return val;
 }
 
+uint32_t read_data_after_address(char *s) {
+
+	uint32_t val = 0;
+
+	while (isspace((int) *s)) {
+		s++;
+	}
+	if (*s == '\0') {
+		return 0;
+	}
+	while (!isspace((int) *s)) {
+		s++;
+	}
+	while (isspace((int) *s)) {
+		s++;
+	}
+	if (*s == '\0') {
+		return 0;
+	}
+	while (*s != '\0') {
+		if (isxdigit((int) *s)) {
+			if (isdigit((int) *s)) {
+				val = val*16 + *s - '0';
+			} else {
+				val = val*16 + toupper(*s) - 'A' + 10;
+			}
+		}
+		s++;
+	}
+	return val;
+}
+
 int main(void)
 {
 
 	char buffer[40] = { 0 };
 	int len;
+	int little = 1;
 
 	usart_init();
 
-	usart_puts("THUAS RISC-V FPGA 32-bit processor\r\n");
+	usart_puts("\r\nTHUAS RISC-V FPGA 32-bit processor\r\n");
 	usart_puts("Monitor v0.1\r\n");
 
 	while (1) {
 
+		usart_puts(">> ");
 		usart_gets(buffer, sizeof buffer);
 		len = strlen(buffer);
 		if (len == 0) {
 			usart_puts("Enter a command\r\n");
 			continue;
 		}
+
+		if (len == 1) {
+			if (buffer[0] == 'h') {
+				usart_puts("Commands:\r\n");
+				usart_puts("h -- this help\r\n");
+				usart_puts("l -- set little endian format\r\n");
+				usart_puts("b -- set big endian format\r\n");
+				usart_puts("rw <address> -- read word\r\n");
+				usart_puts("rh <address> -- read half word\r\n");
+				usart_puts("rb <address> -- read byte\r\n");
+				usart_puts("ww <address> <data> -- write word\r\n");
+				usart_puts("wh <address> <data> -- write half word\r\n");
+				usart_puts("wb <address> <data> -- write byte\r\n");
+				continue;
+			}
+			if (buffer[0] == 'b') {
+				usart_puts("Set Big Endian\r\n");
+				little = 0;
+				continue;
+			}
+			if (buffer[0] == 'l') {
+				usart_puts("Set Little Endian\r\n");
+				little = 1;
+				continue;
+			}
+				
+		}
+
 		if (len < 3) {
 			usart_puts("Enter a correct command\r\n");
 			continue;
 		}
 
 		if (buffer[0] == 'r') {
-			//usart_puts("Here\r\n");
 			if (buffer[1] == 'w') {
 				uint32_t *p = (uint32_t *) read_address(buffer+2);
-				uint32_t val = *p;
-				if ((uint32_t)p % 4 == 0) {
+				if (((uint32_t)p & 3) == 0) {
+					uint32_t val = *p;
+					if (little) {
+						val = ((val & 0xff) << 24) + (((val >> 8) & 0xff) << 16) + (((val >> 16) & 0xff) << 8) + ((val >> 24) & 0xff);
+					}
 					sprintf(buffer, "%08lx", val);
-					usart_puts(">> ");
 					usart_puts(buffer);
 				} else {
 					usart_puts("Not on 4-byte boundary");
 				}
-				usart_puts("\r\n");
 			} else if (buffer[1] == 'h') {
 				uint16_t *p = (uint16_t *) read_address(buffer+2);
-				uint32_t val = *p;
-				if ((uint32_t)p % 2 == 0) {
-					sprintf(buffer, "%04lx", val);
-					usart_puts(">> ");
+				if (((uint32_t)p & 1) == 0) {
+					uint16_t val = *p;
+					if (little) {
+						val = ((val & 0xff) << 8) + ((val >> 8) & 0xff);
+					}
+					sprintf(buffer, "%04lx", (uint32_t) val);
 					usart_puts(buffer);
 				} else {
 					usart_puts("Not on 2-byte boundary");
 				}
-				usart_puts("\r\n");
 			} else if (buffer[1] == 'b') {
 				uint8_t *p = (uint8_t *) read_address(buffer+2);
-				uint32_t val = *p;
-				sprintf(buffer, "%02lx", val);
-				usart_puts(">> ");
+				uint8_t val = *p;
+				sprintf(buffer, "%02lx", (uint32_t) val);
 				usart_puts(buffer);
-				usart_puts("\r\n");
 			} else {
-				usart_puts("Unknown size\r\n");
+				usart_puts("Unknown size");
 			}
-		} else {
-			usart_puts(buffer);
+			usart_puts("\r\n");
+		}
+
+		if (buffer[0] == 'w') {
+			if (buffer[1] == 'w') {
+				volatile uint32_t *p = (uint32_t *) read_address(buffer+2);
+				uint32_t data = read_data_after_address(buffer+2);
+				if (((uint32_t)p & 3) == 0) {
+					*p = data;
+				} else {
+					usart_puts("Not on 4-byte boundary");
+				}
+			} else if (buffer[1] == 'h') {
+				volatile uint16_t *p = (uint16_t *) read_address(buffer+2);
+				uint32_t data = read_data_after_address(buffer+2);
+				if (((uint32_t)p & 1) == 0) {
+					*p = (uint16_t) data;
+				} else {
+					usart_puts("Not on 2-byte boundary");
+				}
+			} else if (buffer[1] == 'b') {
+				volatile uint8_t *p = (uint8_t *) read_address(buffer+2);
+				uint32_t data = read_data_after_address(buffer+2);
+				*p = (uint8_t) data;
+			} else {
+				usart_puts("Unknown size");
+			}
 			usart_puts("\r\n");
 		}
 	}
