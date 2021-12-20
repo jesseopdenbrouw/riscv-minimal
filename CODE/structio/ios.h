bbcae09fdf5b368be6734b2d856f9dc206e800b2
Jesse op den Brouw <51508208+jesseopdenbrouw@users.noreply.github.com>
2021-12-20 10:59:13 +0100

    Make members volatile

--- CODE/structio/ios.h
+++ CODE/structio/ios.h
@@ -9,12 +9,12 @@
 #define IO_BASE (0xf0000000UL)
 
 typedef struct {
-	uint32_t PIN;
-	uint32_t POUT;
+	volatile uint32_t PIN;
+	volatile uint32_t POUT;
 } GPIO_struct_t;
 
 #define GPIOA_BASE (IO_BASE+0x00000000UL)
 
-#define GPIOA ((volatile GPIO_struct_t *) GPIOA_BASE)
+#define GPIOA ((GPIO_struct_t *) GPIOA_BASE)
 
 #endif
