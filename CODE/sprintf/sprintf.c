edf52c6e2b528487fc2c37a6160cf43f5765b072
Jesse op den Brouw <51508208+jesseopdenbrouw@users.noreply.github.com>
2021-12-30 14:49:31 +0100

    Update sprintf.c

--- CODE/sprintf/sprintf.c
+++ CODE/sprintf/sprintf.c
@@ -1,13 +1,15 @@
 #include <stdio.h>
 
-volatile char str[30] = "AAAAAAAAAAAAA";
+volatile char str[50] = "AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA";
 
 volatile int i = 1234;
 
 volatile double j = 1.2;
 
+volatile unsigned long long int k = 0x7fffffffffff;
+
 int main(void)
 {
-	sprintf(str, "%d %f", i, j);
+	sprintf(str, "%d %f %llu", i, j, k);
 }
 
