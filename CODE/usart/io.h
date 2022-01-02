1886d01533a4eab84c66292f7764b1239a92846d
Jesse op den Brouw <51508208+jesseopdenbrouw@users.noreply.github.com>
2022-01-02 12:40:04 +0100

    Update io.h

--- CODE/usart/io.h
+++ CODE/usart/io.h
@@ -1,6 +1,3 @@
-
-
-
 #ifndef _IO_H
 #define _IO_H
 
@@ -8,12 +5,32 @@
 
 #define IO_BASE (0xf0000000UL)
 
-#define GPIOA_PIN (*(volatile uint32_t*)(IO_BASE+0x00000000UL))
+#define GPIOA_PIN  (*(volatile uint32_t*)(IO_BASE+0x00000000UL))
 #define GPIOA_POUT (*(volatile uint32_t*)(IO_BASE+0x00000004UL))
+
+typedef struct {
+        volatile uint32_t PIN;
+        volatile uint32_t POUT;
+} GPIO_struct_t;
+
+#define GPIOA_BASE (IO_BASE+0x00000000UL)
+
+#define GPIOA ((GPIO_struct_t *) GPIOA_BASE)
 
 #define USART_DATA (*(volatile uint32_t*)(IO_BASE+0x00000020UL))
 #define USART_BAUD (*(volatile uint32_t*)(IO_BASE+0x00000024UL))
 #define USART_CTRL (*(volatile uint32_t*)(IO_BASE+0x00000028UL))
 #define USART_STAT (*(volatile uint32_t*)(IO_BASE+0x0000002CUL))
+
+typedef struct {
+	volatile uint32_t DATA;
+	volatile uint32_t BAUD;
+	volatile uint32_t CTRL;
+	volatile uint32_t STAT;
+} USART_struct_t;
+
+#define USART_BASE (IO_BASE+0x00000020UL)
+
+#define USART ((USART_struct_t *) USART_BASE)
 
 #endif
