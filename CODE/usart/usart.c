dd1a2bc68df36cf103920d08c12af09e1345911c
Jesse op den Brouw <51508208+jesseopdenbrouw@users.noreply.github.com>
2022-01-02 12:40:21 +0100

    Use structs to access I/O

--- CODE/usart/usart.c
+++ CODE/usart/usart.c
@@ -3,25 +3,28 @@
 /* Frequency of the DE0-CV board */
 #define F_CPU (50000000UL)
 /* Transmission speed */
-#define BAUD (9600UL)
+#define BAUDRATE (9600UL)
 
 int main(void)
 {
 
 	/* Set baud rate generator */
-	USART_BAUD = F_CPU/BAUD-1;
+	USART->BAUD = F_CPU/BAUDRATE-1;
+
+	/* Set one stop bits */
+	USART->CTRL = 0x00;
 
 	/* Read 8 switches from input */
-	USART_DATA = GPIOA_PIN & 0x000000ff;
+	USART->DATA = GPIOA->PIN & 0x000000ff;
 
 	/* Wait for transmission end */
-	while ((USART_STAT & 0x10) == 0);
+	while ((USART->STAT & 0x10) == 0);
 
 	/* Wait for received character */
-	while ((USART_STAT & 0x04) == 0);
+	while ((USART->STAT & 0x04) == 0);
 
 	/* Put data on leds */
-	GPIOA_POUT = USART_DATA;
+	GPIOA->POUT = USART_DATA;
 
 	return 0;
 }
