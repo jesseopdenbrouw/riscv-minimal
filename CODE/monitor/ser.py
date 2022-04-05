13fb103b4e16d531a1ba1da079dfe95a8f32f13b
Jesse op den Brouw <51508208+jesseopdenbrouw@users.noreply.github.com>
2022-04-05 21:17:55 +0200

    Simple Python3 script to test serial communication with the processor

--- CODE/monitor/ser.py
+++ CODE/monitor/ser.py
@@ -4,12 +4,44 @@
 # you need the serial module
 #
 import serial
-ser = serial.Serial('/dev/ttyUSB0')  # open serial port
-print(ser.name)         # check which port was really used
-line = ser.readline()
-print(line)
-ser.write(b'hello\r\n')     # write a string
-line = ser.readline()
-print("%s" % line)
+import sys
+
+port = "/dev/ttyUSB0"
+
+try:
+    ser = serial.Serial(port, baudrate=9600, timeout=1)  # open serial port
+
+except:
+    print("Error opening device!")
+    sys.exit("Bailing out!")
+
+print("Device = %s" % ser.name)         # check which port was really used
+
+try: 
+    line = ser.readline()
+
+except:
+    print("Error reading from device!")
+    sys.exit("Bailing out!")
+
+sys.stdout.buffer.write(line)
+
+try:
+    ser.write(b'hello\r\n')     # write a string
+
+except:
+    print("Error writing to device!")
+    ser.close()             # close port
+    sys.exit("Bailing out!")
+
+try: 
+    line = ser.readline()
+
+except:
+    print("Error reading from device!")
+    ser.close()             # close port
+    sys.exit("Bailing out!")
+
+sys.stdout.buffer.write(line)
 ser.close()             # close port
 
